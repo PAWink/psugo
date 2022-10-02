49b52e97cfce69c87a704d0d6f272f70edbb38ea
PAWink <mysisterwinkwink@gmail.com>
2022-10-03 00:10:39 +0700

    request

--- lib/states/posted.dart
+++ lib/states/posted.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:psugo/states/sendrequest.dart';
 import 'package:psugo/utility/my_constant.dart';
 import 'package:psugo/widgets/show_title.dart';
 
@@ -12,15 +13,44 @@ class Posted extends StatefulWidget {
 class _PostedState extends State<Posted> {
   @override
   Widget build(BuildContext context) {
+    //size
+    double size = MediaQuery.of(context).size.width;
     return Scaffold(
         backgroundColor: Colors.blue[100],
         body: SafeArea(
           child: ListView(
             children: [
               buildPosted(),
+              buildChoosePost(size),
             ],
           ),
         ));
+  }
+
+  Row buildChoosePost(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          color: Colors.black,
+          width: size * 0.6,
+          margin: EdgeInsets.all(60),
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Sendrequest(),
+                  ));
+            },
+            child: Text(
+              'Start from PSU Phuket , Time to start: 14.30',
+              style: MyConstant().h2text(),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 
   Row buildPosted() {
---
+++ lib/states/sendrequest.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Sendrequest extends StatefulWidget {
+  const Sendrequest({super.key});
+
+  @override
+  State<Sendrequest> createState() => _SendrequestState();
+}
+
+class _SendrequestState extends State<Sendrequest> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
